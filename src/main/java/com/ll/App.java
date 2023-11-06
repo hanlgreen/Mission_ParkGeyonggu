4b43d48fc59696e2efce1482370b1642d4b9dbb1
hanlgreen <luckygoldsta@naver.com>
2023-11-06 11:45:32 +0900

    stage2 complete

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -12,11 +12,15 @@ public class App {
             Scanner scanner = new Scanner(System.in);
             String cmd = scanner.nextLine();
 
-            if(cmd.equals("종료")) {
+            if (cmd.equals("종료")) {
                 break;
+            } else if (cmd.equals("등록")) {
+                System.out.println("명언 : ");
+                System.out.println("작가 : ");
             }
-
-            System.out.printf("입력받은 명령어 : %s\n", cmd);
         }
     }
 }
+
+
+
